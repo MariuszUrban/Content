887c9f575f2f96c081fc4858069941b2c95586fd
Mariusz <urban.mariusz@icloud.com>
2021-05-20 13:26:51 +0200

    działa kurwa

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import textSearchReducer from "./Searchers/textSearch/TextSearchSlice";
 import feelingsSearchReducer from "./Searchers/feelingsSearch/FeelingsSearchSlice";
+import styleSearchReducer from "./Searchers/styleSearch/StyleSearchSlice";
 
 export default configureStore({
   reducer: {
     text: textSearchReducer,
     feelings: feelingsSearchReducer,
+    style: styleSearchReducer,
   },
 });
